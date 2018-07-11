4e719101f43c264405bb4d3e8f5f1596e5de8410
Loide Jesus <loide.jesus@tpv-tech.com>
2018-07-11 15:09:35 -0400

    Update solution for Array left rotation

--- cracking_the_coding_interview/array_left_rotation.py
+++ cracking_the_coding_interview/array_left_rotation.py
@@ -1,3 +1,17 @@
+"""
+Arrays: Left Rotation
+Check out the resources on the page's right side to learn more about arrays. 
+The video tutorial is by Gayle Laakmann McDowell, author of the best-selling 
+interview book Cracking the Coding Interview.
+
+A left rotation operation on an array shifts each of the array's elements 1 
+unit to the left. For example, if 2 left rotations are performed on array 
+[1,2,3,4,5], then the array would become [3,4,5,1,2].
+
+Given an array a of n integers and a number, d, perform d left rotations on the
+array. Return the updated array to be printed as a single line of 
+space-separated integers.
+"""
 def rotLeft(a, d):
     arr_size = len(a)
     startpoint = d % arr_size
